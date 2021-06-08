c27912c9a26a7d71272dac8f794cc7a849e966d2
jammaGlobal <jam2evos@hotmail.com>
2021-06-08 15:28:44 +1000

    checking further

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const featuredProjects = [
         title: "Software Quality Metrics Tool",
         para: "The QualiSoft Tool system provides the user with a quality analysis of their Java code based on certain quality metrics deemed valuable by the software team. Input options allow user to specify metrics to receive analaysis from. Output options allow software team to add output formats.",
         img:
-            "./portfolio-site/assets/qualisoft2.PNG",
+            "./assets/qualisoft2.PNG",
         link: "https://github.com/jammaGlobal/SENG4430-SOFTWARE-QUALITY_",
     },
     {
@@ -12,7 +12,7 @@ export const featuredProjects = [
         title: "Lexer and Parser for CD20 Toy Language",
         para: "This is a to-be finished compiler written in Java for a toy language called CD20 that is to be run in a toy stack machine called SM20. The Lexical Analysis and the Parsing using 'recursive descent' has been completed and tested.",
         img:
-            "./portfolio-site/assets/compiler.PNG",
+            "./assets/compiler.PNG",
         link: "https://github.com/jammaGlobal/COMP3290-CompilerDesign"
         
     },
@@ -30,7 +30,7 @@ export const featuredProjects = [
         title: "Graph Theory Web App",
         para: "A NodeJs application that inputs a node name, distance, and a JSON file describing a set of nodes, each with an associated name, coordinates, and the other nodes to which it is connected. Outputs a visualisation using p5 display specific nodes based on the input conditions given. Full explanation of the app in the repo description.",
         img:
-            ".\portfolio-site\assets\graphapp.png",
+            "./assets/graphapp.png",
         link: "https://github.com/jammaGlobal/shortestPathWebApp"
 
     },
@@ -39,7 +39,7 @@ export const featuredProjects = [
         title: "Time Zone Web App",
         para: "An ASP.NET Core application that inputs a country name and returns the time in that country.",
         img:
-            ".\assets\timezone.png",
+            "./assets/timezone.png",
         link: "https://github.com/jammaGlobal/GloboTime"
 
     }
