c64361270524533e8705a8a5366283626c26ccc1
jammaGlobal <jam2evos@hotmail.com>
2021-06-16 12:14:49 +1000

    changes for user to rebuild project

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const featuredProjects = [
         title: "Software Quality Metrics Tool",
         para: "The QualiSoft Tool system provides the user with a quality analysis of their Java code based on certain quality metrics deemed valuable by the software team. Input options allow user to specify metrics to receive analaysis from. Output options allow software team to add output formats.",
         img:
-            "./assets/qualisoft2.PNG",
+            "./portfolio-site/assets/qualisoft2.PNG",
         link: "https://github.com/jammaGlobal/SENG4430-SOFTWARE-QUALITY_",
     },
     {
@@ -19,7 +19,7 @@ export const featuredProjects = [
         title: "Lexer and Parser for CD20 Toy Language",
         para: "This is a to-be finished compiler written in Java for a toy language called CD20 that is to be run in a toy stack machine called SM20. The Lexical Analysis and the Parsing using 'recursive descent' has been completed and tested.",
         img:
-            "./assets/compiler.PNG",
+            "./portfolio-site/assets/compiler.PNG",
         link: "https://github.com/jammaGlobal/COMP3290-CompilerDesign"
         
     },
@@ -28,7 +28,7 @@ export const featuredProjects = [
         title: "Machine Learning Tasks",
         para: "Projects are written in python/ipynb, reports feature interpretation of results including performance metrics and confusion matrices. Current completed tasks: MultiVar Classifier for Statlog (Shuttle) dataset, MultiVar Classifier for Iris dataset.",
         img:
-            "./assets/nnmachinelearning.png",
+            "./portfolio-site/assets/nnmachinelearning.png",
         link: "https://github.com/jammaGlobal/machineLearningTasks"
         
     },
@@ -37,7 +37,7 @@ export const featuredProjects = [
         title: "Graph Theory Web App",
         para: "A NodeJs application that inputs a node name, distance, and a JSON file describing a set of nodes, each with an associated name, coordinates, and the other nodes to which it is connected. Outputs a visualisation using p5 display specific nodes based on the input conditions given. Full explanation of the app in the repo description.",
         img:
-            "./assets/graphapp.png",
+            "./portfolio-site/assets/graphapp.png",
         link: "https://github.com/jammaGlobal/shortestPathWebApp"
 
     },
@@ -46,7 +46,7 @@ export const featuredProjects = [
         title: "Time Zone Web App",
         para: "An ASP.NET Core application that inputs a country name and returns the time in that country.",
         img:
-            "./assets/timezone.png",
+            "./portfolio-site/assets/timezone.png",
         link: "https://github.com/jammaGlobal/GloboTime"
 
     }
@@ -57,8 +57,8 @@ export const blogPosts = [
         id: 1,
         title: "Critique of the Conceptual Framework of Autonomous Vehicles",
         para: "Autonomous vehicles are one of the fore runners of AI consumer technologies in the circulated media, with companies like Tesla, Waymo, Uber, as well as traditional car manufacturers competing to successfully develop a car that will not require any human attention while driving. This paper attempts to perform clarifcation of the conceptual framework taken in by researchers and engineers when designing these AI pipelines for autonomous driving, specifcally perception mechanisms. One cause of the conceptual incoherence is postulated to be the rapidly demanding research and development driven by the speculative business model GAFA companies occupy.",
-        link: "./assets/hiPhiPaper.pdf",
-        img: "./assets/chariotSmall.jpg",
+        link: "./portfolio-site/assets/hiPhiPaper.pdf",
+        img: "./portfolio-site/assets/chariotSmall.jpg",
         date: "7/06/21"
     }
 
